b9fc1ab39ff22f37d42bcfb2dc086d5c11d99365
Will Mansfield <william.mansfield@mymail.champlain.edu>
2026-04-25 16:57:14 -0400

    fixed all build setting OMG

--- Gore/Source/Gore/Gore.Build.cs
+++ Gore/Source/Gore/Gore.Build.cs
@@ -4,41 +4,44 @@ using UnrealBuildTool;
 
 public class Gore : ModuleRules
 {
-	public Gore(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public Gore(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core",
-			"CoreUObject",
-			"Engine",
-			"InputCore",
-			"EnhancedInput",
-			"AIModule",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
-			"UMG",
-			"Slate"
-		});
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "AIModule",
+            "StateTreeModule",
+            "GameplayStateTreeModule",
+            "UMG",
+            "Slate",
+            "GeometryScriptingCore",
+            "GeometryCore",
+            "GeometryFramework"
+        });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { });
+        PrivateDependencyModuleNames.AddRange(new string[] { });
 
-		PublicIncludePaths.AddRange(new string[] {
-			"Gore",
-			"Gore/Variant_Horror",
-			"Gore/Variant_Horror/UI",
-			"Gore/Variant_Shooter",
-			"Gore/Variant_Shooter/AI",
-			"Gore/Variant_Shooter/UI",
-			"Gore/Variant_Shooter/Weapons"
-		});
+        PublicIncludePaths.AddRange(new string[] {
+            "Gore",
+            "Gore/Variant_Horror",
+            "Gore/Variant_Horror/UI",
+            "Gore/Variant_Shooter",
+            "Gore/Variant_Shooter/AI",
+            "Gore/Variant_Shooter/UI",
+            "Gore/Variant_Shooter/Weapons"
+        });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
